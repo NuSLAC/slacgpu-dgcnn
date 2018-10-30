070480c919bf71c25fecc1600b0bcdc7589e78ee
drinkingkazu <kazuhiro@nevis.columbia.edu>
2018-10-29 20:15:41 -0700

    well let's expose everything

--- train_dgcnn.sh
+++ train_dgcnn.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
+invalid=0;
+for arg in "$@"
+do
+    if [ "$arg" = "-if" ]; then
+	invalid=1;
+    elif [ "$arg" = "-ld" ]; then
+	invalid=1;
+    elif [ "$arg" = "-wp" ]; then
+	invalid=1;
+    fi
+    if [ $invalid -eq 1 ]; then
+	echo "This script cannot take the argument $arg";
+	exit;
+    fi
+done
 
-MODEL_NAME=$1;  # [dgcnn,residual-dgcnn,residual-dgcnn-nofc]
-NUM_LAYERS=$2;  # number of edge convolution layers
-NUM_FILTERS=$3; # number of filters in each edge convolution layer
-K1=$4;          # K1 factor in loss_same_pdg in clustering dgcnn
-K2=$5;          # K2 factor in loss_diff_group in clustering dgcnn
-ALPHA=$6;       # Alpha factor for loss_same_pdg
-ITERATION=$7;   # number of iterations
-
-ALPHA_DECAY=20;
-KVALUE=40;
-
+ARGS="$*"
 #CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 IN_STORAGE_DIR=/gpfs/slac/staas/fs1/g/neutrino/kterao;
 OUT_STORAGE_DIR=/gpfs/slac/staas/fs1/g/neutrino/${USER};
@@ -26,8 +31,8 @@ mkdir -p $DATA_DIR;
 cp $IN_STORAGE_DIR/data/$TRAIN_FILE $DATA_DIR;
 cd $WORK_DIR;
 echo $CUDA_VISIBLE_DEVICES >> log.txt
-echo $DGCNN_DIR/bin/dgcnn.py train -bs 24 -mbs 1 -np -1 --gpus $CUDA_VISIBLE_DEVICES -chks 1000 -chkn 1 -chkh 0.2 -ss 100 -rs 50 -it $ITERATION -ld log -wp weights/snapshot -kv $KVALUE -mn $MODEL_NAME -ecl $NUM_LAYERS -ecf $NUM_FILTERS -io larcv -dkeys data,sim_group,sim_pdg  -if $DATA_DIR/$TRAIN_FILE -k1 $K1 -k2 $K2 -al $ALPHA -ad $ALPHA_DECAY >> log.txt;
-$DGCNN_DIR/bin/dgcnn.py train -bs 24 -mbs 1 -np -1 --gpus $CUDA_VISIBLE_DEVICES -chks 1000 -chkn 1 -chkh 0.2 -ss 100 -rs 50 -it $ITERATION -ld log -wp weights/snapshot -kv $KVALUE -mn $MODEL_NAME -ecl $NUM_LAYERS -ecf $NUM_FILTERS -io larcv -dkeys data,sim_group,sim_pdg -if $DATA_DIR/$TRAIN_FILE -k1 $K1 -k2 $K2 -al $ALPHA -ad $ALPHA_DECAY >> log.txt;
+echo $DGCNN_DIR/bin/dgcnn.py $ARGS -ld log -wp weights/snapshot -if $DATA_DIR/$TRAIN_FILE >> log.txt;
+$DGCNN_DIR/bin/dgcnn.py $ARGS -ld log -wp weights/snapshot -if $DATA_DIR/$TRAIN_FILE >> log.txt;
 cd ..;
 mkdir -p $OUT_STORAGE_DIR;
 cp -r $WORK_DIR $OUT_STORAGE_DIR;
@@ -38,4 +43,3 @@ if [ $? -eq 0 ]; then
 rm -rf $WORK_DIR;
 fi
 rm -rf $DATA_DIR;
-
